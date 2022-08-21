b0cfcb12ec1a47089f0bc2413b43b8ccf5238236
Floris de Vries <florisdev@hotmail.com>
2022-08-21 09:28:45 +0200

    Disable renderers option for transforms

---
+++ Runtime/Extensions/TransformExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FlorisDeVToolsUnityExtensions.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void DisableRenderersRecursive(this Transform parent)
+        {
+            if (parent.TryGetComponent<Renderer>(out var objRenderer))
+            {
+                objRenderer.enabled = false;
+            }
+
+            foreach (Transform child in parent)
+            {
+                DisableRenderersRecursive(child);
+            }
+        }
+    }
+}
